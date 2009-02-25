c55f788a1cb5fab81a5a3925fda09aa24ae35605
Charles Romestant <cromestant@gmail.com>
2009-02-25 16:08:10 -0430

    colours! Ansi colours !!!!!

--- twitminal.py
+++ twitminal.py
@@ -12,61 +12,66 @@ password=''
 mychecker=None
 esc = '%s['%chr(27)
 reset = '%s0m'%esc
+red ='%s31m'%esc
+blue ='%s36m'%esc
+green ='%s32m'%esc
 
-
+def printc(color,text):
+	print color,text,reset
+	
 def printHelpMenu():
-	print "Twitminal- by Charz"
-	print "--help menu:"
-	print "--Available commands :"
-	print "  --help (prints this menu)"
-	print "  --last (shows last non-empty import of twits)"
-	print "  --direct:<name>:<message> (posts direct message to user)"
-	print "  --friendsList (prints friends list)"
-	print "  exit() (Kills notifier thread, then quits)"
+	printc(reset,"Twitminal- by Charz")
+	printc (green,"--help menu:")
+	printc (green,"--Available commands :")
+	printc (green,"  --help (printcs this menu)")
+	printc (green,"  --last (shows last non-empty import of twits)")
+	printc (green,"  --direct:<name>:<message> (posts direct message to user)")
+	printc (green,"  --friendsList (printcs friends list)")
+	printc (green,"  exit() (Kills notifier thread, then quits)")
 
 def handler(signum, frame):
-	print "Killing thread"
+	printc (red,"Killing thread")
 	mychecker.stopNdone()
 	while mychecker.isAlive():
-		print "Thread still alive, wait a tick!"
+		printc (red,"Thread still alive, wait a tick!")
 		sleep(3)
-	print "Bye bye"
+	printc (reset,"Bye bye")
 	sys.exit()
 
-print reset,"Welcome! Twitminal, minimal twitter client with growl notifications!\nBy Charz! (Charles Romestant)"
-print "cromestant@gmail.com\n\n"
-print "Twitter username: "
+print("Welcome! Twitminal, minimal twitter client with growl notifications!\nBy Charz! (Charles Romestant)")
+printc (blue,"cromestant@gmail.com\n\n")
+print("Twitter username: ")
 username = sys.stdin.readline()[:-1]
-print "Twitter Password: "
+print("Twitter Password: ")
 password=sys.stdin.readline()[:-1]
 
 try:
 	api=twitter.Api(username,password,'utf-8')
 	friends = api.GetFriends()
 except:
-	print "Authorization error\nor comunication errors"
+	printc (red,"Authorization error\nor comunication errors")
 	sys.exit()
-print "Type 'quit()' to exit --help for commands\nAnything else WILL be twitted!\n"
+printc (green,"Type 'quit()' to exit --help for commands\nAnything else WILL be twitted!\n")
 signal.signal(signal.SIGINT, handler)
 mychecker = checker.checker(username,password)
 mychecker.start()
 loop= True
 while loop:
-	print "Twitminal : #"
+	print("Twitminal : #")
 	text = sys.stdin.readline()[:-1]
 	if text=='quit()' or text=='exit()':
 		mychecker.stopNdone()
 		loop =False
 		while mychecker.isAlive():
-			print "Thread still alive, wait a tick!"
+			printc (red,"Thread still alive, wait a tick!")
 			sleep(3)
-		print "Bye bye"
+		print("Bye bye")
 	elif text.startswith("--help"):
 		printHelpMenu()
 	elif text.startswith("--last"):
 		lastl = mychecker.getLastLine()
 		for f in lastl:
-			print f.GetUser().screen_name+": "+f.GetText()
+			printc (blue,f.GetUser().screen_name+": "+reset+f.GetText())
 	elif text.startswith("--direct:"):
 		x= text.partition(":")[2].partition(":")
 		if len(x[2])<141:
@@ -74,9 +79,9 @@ while loop:
 	elif text=="--friendsList":
 		friends = api.GetFriends(username)
 		for f in friends:
-			print f.screen_name
+			printc(blue, f.screen_name)
 	elif len(text)>140:
-		print "over 140 characters.."
+		printc (red,"over 140 characters..")
 	else:
 		#send twitt!
 		api.PostUpdate(text)
